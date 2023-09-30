daddc77c523d40760c7fbf9dc937b7b360e5af2d
jiangcunyou <jiangbowen0112@hotmail.com>
2023-09-30 18:50:35 -0400

    react - state

--- react-course/src/components/Expenses/Expenses.js
+++ react-course/src/components/Expenses/Expenses.js
@@ -1,10 +1,22 @@
+import React, { useState } from 'react';
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+
+    const [filteredYear, setFilteredYear] = useState('2020')
+    const filterChangeHandler = (selectedYear) => {
+        console.log('Expense.js');
+        console.log(selectedYear);
+        setFilteredYear(selectedYear);
+    };
+
     return (
+        <div>
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
             <ExpenseItem
                 expense={props.items[0]}/>
             <ExpenseItem
@@ -14,6 +26,7 @@ function Expenses(props) {
             <ExpenseItem
                 expense={props.items[3]}/>
         </Card>
+        </div>
     );
 }
 
