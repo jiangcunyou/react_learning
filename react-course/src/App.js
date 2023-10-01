66a7e34f325985fbdf0f6b4be99dcf0f79613f31
jiangcunyou <jiangbowen0112@hotmail.com>
2023-10-01 19:28:22 -0400

    react - list

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,36 +1,39 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
-const App = () => {
-    const expenses = [
-        {
-            id: 'e1',
-            title: 'Car Insurance',
-            amount: '294.67',
-            date: new Date(2023,2,23)
-        },
-        {
-            id: 'e2',
-            title: 'Lion Computing',
-            amount: '55.21',
-            date: new Date(1996,4,19)
-        },
-        {
-            id: 'e3',
-            title: 'Zoo Keeper',
-            amount: '221.2',
-            date: new Date(2001,9,12)
-        },
-        {
-            id: 'e4',
-            title: 'Beer convention',
-            amount: '11.2',
-            date: new Date(2011,5,1)
-        }
-    ];
+import {useState} from "react";
 
+const DUMMY_EXEPENSES = [
+    {
+        id: 'e1',
+        title: 'Car Insurance',
+        amount: '294.67',
+        date: new Date(2022,2,23)
+    },
+    {
+        id: 'e2',
+        title: 'Lion Computing',
+        amount: '55.21',
+        date: new Date(2021,4,19)
+    },
+    {
+        id: 'e3',
+        title: 'Zoo Keeper',
+        amount: '221.2',
+        date: new Date(2021,9,12)
+    },
+    {
+        id: 'e4',
+        title: 'Beer convention',
+        amount: '11.2',
+        date: new Date(2019,5,1)
+    }
+];
+const App = () => {
+    const [expenses, setExpenses] = useState(DUMMY_EXEPENSES);
     const addExpenseHandler = (expense) => {
-        console.log('In App.js');
-        console.log(expense);
+        setExpenses(prevExpenses => {
+            return [expense, ...prevExpenses];
+        });
     }
 
   return (
--- react-course/src/components/Expenses/ExpenseItem.js
+++ react-course/src/components/Expenses/ExpenseItem.js
@@ -5,21 +5,23 @@ import {useState} from "react";
 
 const ExpenseItem = (props) => {
 
-    const [title, setTitle] = useState(props.expense.title);
+    const [title, setTitle] = useState(props.title);
 
     const clickHandler = () => {
         setTitle('Updated!');
     }
 
     return (
-        <Card className="expense-item">
-            <ExpenseDate date={props.expense.date}/>
-            <div className="expense-item__description">
-                <h2>{title}</h2>
-                <div className="expense-item__price">${props.expense.amount}</div>
-            </div>
-            <button onClick={clickHandler}>Change Title</button>
-        </Card>
+        <li>
+            <Card className="expense-item">
+                <ExpenseDate date={props.date}/>
+                <div className="expense-item__description">
+                    <h2>{title}</h2>
+                    <div className="expense-item__price">${props.amount}</div>
+                </div>
+                <button onClick={clickHandler}>Change Title</button>
+            </Card>
+        </li>
     );
 }
 
