a05556a583ceb80b3a8779d3e0e5805a362c9a43
jiangcunyou <jiangbowen0112@hotmail.com>
2024-02-03 15:25:46 -0500

    FEB 3RD, 2024

--- 01-starting-project-styleing/src/components/Header.jsx
+++ 01-starting-project-styleing/src/components/Header.jsx
@@ -1,15 +1,22 @@
 import logo from '../assets/logo.png';
-import './Header.css'
+// import classes from './Header.module.css'
+import {styled} from "styled-components";
+
+const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+`
 
 export default function Header() {
   return (
-    <header>
+    <StyledHeader>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p style={{
-          color: 'red',
-          textAlign: "center"
-      }}>A community of artists and art-lovers.</p>
-    </header>
+      <p>A community of artists and art-lovers.</p>
+    </StyledHeader>
   );
 }
